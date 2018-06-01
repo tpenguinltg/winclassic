2c8d84f09792aff0cc565d937c528abcb9d56b1c
tPenguinLTG <tpenguinltg@disroot.org>
2018-06-01 14:25:50 -0400

    Support export to INI
    The output of this export function can be used directly in a .theme
    file's [Control Panel\Colors] section.

--- js/winclassic.js
+++ js/winclassic.js
@@ -2,6 +2,7 @@ function WinClassicTheme() {
   Theme.call(this);
 
   var pickers = document.getElementsByClassName("color-item");
+  var exportDestination = document.getElementById("export");
 
   for (var i = 0; i < pickers.length; i++) {
     var picker = pickers[i];
@@ -9,16 +10,32 @@ function WinClassicTheme() {
     this.updateFromStylesheet(itemName);
     picker.value = this.getItemColor(itemName);
     picker.oninput = this.onColorChange.bind(this);
+    picker.onchange = function() {
+      exportDestination.value = this.exportToIni();
+    }.bind(this);
   }
+
+  exportDestination.value = this.exportToIni();
 
   return this;
 }
 
 Object.setPrototypeOf(WinClassicTheme.prototype, Theme.prototype);
 
-WinClassicTheme.prototype.onColorChange = function(e) {
+WinClassicTheme.prototype.onColorChange = function(exportDestination, e) {
   var name = e.target.dataset.item;
   var color = e.target.value;
   this.setItemColor(name, color);
   this.updateStylesheet(name);
+  exportDestination.value = this.exportToIni();
+}
+
+WinClassicTheme.prototype.exportToIni = function() {
+  var ini = "";
+  for (var item in this.items) {
+    var rgb = window.normalizeColor.rgba(window.normalizeColor(this.items[item].color));
+    ini += item + "=" + rgb.r + " " + rgb.g + " " + rgb.b + "\n";
+  }
+
+  return ini.trim();
 }
